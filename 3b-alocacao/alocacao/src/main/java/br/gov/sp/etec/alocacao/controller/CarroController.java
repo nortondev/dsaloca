46b33883779fbd86b6f6186c50610e159651e4b4
3bdsmod <3bdsmod@bglab.local>
2023-04-18 20:37:28 -0300

    Atualizando Métodos

--- 3b-alocacao/alocacao/src/main/java/br/gov/sp/etec/alocacao/controller/CarroController.java
+++ 3b-alocacao/alocacao/src/main/java/br/gov/sp/etec/alocacao/controller/CarroController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +27,19 @@ public class CarroController {
 	}
 
 	@PostMapping("adicionar-carro")
-	public Carro addCarro(@RequestBody Carro carro) { // get = url, post = vem no body
+	public Carro addCarro(@RequestBody Carro carro) {  // get = url, post = vem no body
 		return repository.save(carro);
 	}
+	
+	@PutMapping("atualizar-carro")
+	public Carro updateCarro(@RequestBody Carro carro) {
+		return repository.save(carro); // recuperar o body
+	}
+	
+	@DeleteMapping("deletar-carro/{id}")
+	public void deleteCarro(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+
+
 }
--- 3b-alocacao/alocacao/src/main/java/br/gov/sp/etec/alocacao/controller/ClienteController.java
+++ 3b-alocacao/alocacao/src/main/java/br/gov/sp/etec/alocacao/controller/ClienteController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,15 +21,26 @@ public class ClienteController {
 	
 	@Autowired
 	ClienteRepository repository;
-	@GetMapping("/clientes")
+	@GetMapping("/clientes") // pegar lista;uma consulta
 	public List<Cliente> clientes() {
 		return repository.findAll();
 		
 	}
 	
-	@PostMapping("adicionar-cliente")
+	@PostMapping("adicionar-cliente") //enviando os arquivos
 	public Cliente addCliente(@RequestBody Cliente cliente)
 	{
 		return repository.save(cliente);
+	}
+	
+	@PutMapping("atualizar-cliente")
+	public Cliente updateCliente(@RequestBody Cliente cliente)
+	{
+		return repository.save(cliente);
+	}
+	
+	@DeleteMapping("deletar-cliente/{id}")
+	public void deleteCliente(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
